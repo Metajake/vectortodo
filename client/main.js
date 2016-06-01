6a6be03835b048a2f4a4782387db09fc5befb496
Metajake <jaynoco@gmail.com>
2016-06-01 16:34:18 -0400

    Task view filter, delete all button

--- client/main.js
+++ client/main.js
@@ -1,1 +1,27 @@
 import '../imports/ui/body.js';
+
+$(document).ready(function(){
+    $('#filter').change(function(event){
+        console.log($('#filter').val())
+        const value = $('#filter').val();
+        switch(value){
+            case("all"):
+                $('.task').show();
+                break;
+            case('active'):
+                $('.checked').hide();
+                $('.task:not(.checked)').show();
+                break;
+            case('completed'):
+                $('.checked').show();
+                $('.task:not(.checked)').hide();
+            default:
+                console.log("defaiult")
+        }
+
+        //$('.checked').toggle()
+    });
+
+});
+//console.log($('#hideCompleted'))
+//$('.checked').css({visibility:'none'})
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,15 +1,24 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { Tasks } from '../api/tasks.js';
 
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated(){
+    this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
     tasks() {
+        const instance = Template.instance();
+        if(instance.state.get('hideCompleted')){
+            return Tasks.find({checked: { $ne: true} }, {sort: {createdAt: -1 } });
+        }
         return Tasks.find({}, {sort: {createdAt: -1 } });
     },
     count() {
-        return Tasks.find({}).count();
+        return Tasks.find({checked: { $ne: true} }).count();
     },
 });
 
@@ -26,5 +35,11 @@ Template.body.events({
         });
 
         target.text.value = '';
+    },
+    'click .deleteAll'() {
+        const toDelete = Tasks.find({checked: true});
+        toDelete.forEach(function(task){
+            Tasks.remove({"_id": task._id})
+        })
     },
 });
